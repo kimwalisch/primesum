708c11a9e5f3ab58436f796a1bc1eddc11c3232c
Kim Walisch <kim.walisch@gmail.com>
2018-05-01 21:30:47 +0200

    Refactor

--- include/imath.hpp
+++ include/imath.hpp
@@ -68,7 +68,6 @@ inline int ilog(T x)
   return (int) std::log((double) x);
 }
 
-/// Raise to power
 template <typename T>
 inline T ipow(T x, int n)
 {
@@ -89,23 +88,17 @@ inline T iroot(T x)
   T r = (T) std::pow((double) x, 1.0 / N);
 
   // fix root too large
-  for (; r > 0 && ipow(r, N - 1) > x / r; r--);
+  for (; r > 0; r--)
+  {
+    if (ipow(r, N - 1) <= x / r)
+      break;
+  }
 
   // fix root too small
-  for (; ipow(r + 1, N - 1) <= x / (r + 1); r++);
+  while (ipow(r + 1, N - 1) <= x / (r + 1))
+    r += 1;
 
   return r;
-}
-
-/// Count the number of primes <= x using binary search.
-/// @pre primes[1] = 2, primes[3] = 3, ...
-/// @pre x <= primes.back()
-///
-template <typename T1, typename T2>
-inline T2 pi_bsearch(const std::vector<T1>& primes, T2 x)
-{
-  assert(primes[0] == 0);
-  return (T2) (std::upper_bound(primes.begin() + 1, primes.end(), x) - (primes.begin() + 1));
 }
 
 template <typename T1, typename T2, typename T3>
@@ -117,6 +110,18 @@ inline T2 in_between(T1 min, T2 x, T3 max)
     return (T2) max;
 
   return x;
+}
+
+/// Count the number of primes <= x using binary search.
+/// @pre primes[1] = 2, primes[3] = 3, ...
+/// @pre x <= primes.back()
+///
+template <typename T1, typename T2>
+inline T2 pi_bsearch(const std::vector<T1>& primes, T2 x)
+{
+  assert(primes.size() < 2 || primes[1] == 2);
+  auto start = primes.begin() + 1;
+  return (T2) (std::upper_bound(start, primes.end(), x) - start);
 }
 
 } // namespace
